4384e6357161e916455a3075dcc5510dff0dfab9
vishal singh <vishurajputmgr@gmail.com>
2022-09-25 21:45:52 +0530

    reset button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 // import Product from './components/Product';
 import ProductList from './components/ProductList'
 import Footer from './components/Footer'
@@ -11,22 +11,22 @@ function App() {
     {
       price: 99999,
       name: 'iphone ultra pro max',
-      quantity: 10,
+      quantity: 0,
     },
     {
       price: 9999,
       name: 'redmenote 10 ultra pro max',
-      quantity: 20,
+      quantity: 0,
     },
     {
       price: 8799,
       name: 'Mi ultra pro max',
-      quantity: 50,
+      quantity: 0,
     },
     {
       price: 99999,
       name: 'realme 11 ultra pro max',
-      quantity: 90,
+      quantity: 0,
     },
   ]
 
@@ -41,10 +41,21 @@ function App() {
 
   const decrementQuantity = (index)=>{
     let newProductList = [...productList]
-    // let newTotalAmount = [...totalAmount]
-    newProductList[index].quantity > 0 ? newProductList[index].quantity-- : newProductList[index]
+    newProductList[index].quantity > 0 && newProductList[index].quantity--
     setProductList(newProductList)
-    setTotalAmount(newAmount)
+  }
+
+  useEffect(() => {
+    setTotalAmount(productList.map(prod => prod.price * prod.quantity).reduce((acc, price) => acc + price, 0))
+  }, [productList])
+
+  const resetQuantity = () => {
+    let newProductList = [...productList]
+    newProductList.map((product) => {
+      product.quantity = 0
+    })
+    setProductList(newProductList)
+    setTotalAmount(0)
   }
 
   
@@ -54,7 +65,7 @@ function App() {
     <main className="container mt-5"> 
     <ProductList productList={productList} incrementQuantity = {incrementQuantity}  decrementQuantity = {decrementQuantity} />
     </main>
-    <Footer totalAmount ={totalAmount} />
+    <Footer totalAmount = {totalAmount} resetQuantity = {resetQuantity}/>
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer(props) {
   return (
     <div className ="row fixed-bottom">
-        <button className="btn btn-danger col-2">Reset</button>
-        <div className="col-8 bg-dark text-white text-center">
+        <button className="btn btn-danger col-2" onClick={() => { props.resetQuantity()}}>Reset</button>
+        <div className="col-8 bg-dark text-white text-center"> Total-  ₹
             {props.totalAmount}
         </div>
         <button className="btn btn-primary col-2">Pay Now</button>
